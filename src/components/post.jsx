d116690c622a5d5db2bdbe3e324967cc6168554d
Duru C. Mikael <durumikaelc@gmail.com>
2022-12-01 15:24:33 +0100

    update anchor tag class to claasName

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 const Post = ({ post }) => {
   return (
     <section className="container mx-auto px-2 md:px-3 lg:px-8 py-5">
@@ -9,11 +7,11 @@ const Post = ({ post }) => {
           <div className="p-5">
             <h2 className="my-3 text-2xl font-bold tracking-tight text-gray-900">{post.title}</h2>
             <p className="font-normal text-gray-70 mb-5">{post.brief}</p>
-            <Link 
-              to={`https://mikael.hashnode.dev/${post.slug}`} className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+            <a 
+              href={`https://mikael.hashnode.dev/${post.slug}`} className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
               Read more
               <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </Link>
+            </a>
           </div>
         </div>
       </div>
